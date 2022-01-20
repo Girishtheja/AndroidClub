181ec43c3fa416bae9bddce5a446a71be09aba1d
Girish Theja <girishtheja116@gmail.com>
2022-01-20 23:44:45 +0530

    Assignments

---
+++ SecondAssignment/SecondAssign/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SignUp from './src/screens/SignUp';
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <SignUp/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ SecondAssignment/SecondAssign/src/screens/SignUp.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View ,TextInput, Button } from 'react-native';
+
+export default function SignUp() {
+  return (
+    <View style={styles.container}>
+    <Text style={{fontWeight:'bold',fontSize:40,paddingBottom:30}}>SignUp</Text>
+    <TextInput style={styles.inputs} placeholder='Full Name'></TextInput>
+    <TextInput style={styles.inputs} placeholder='E-Mail Address'></TextInput>
+    <TextInput className="MARGIN" secureTextEntry='true' style={[styles.inputs,{marginBottom:20}]} placeholder='Password'></TextInput>
+    
+    <Button 
+    title='SIGNUP' 
+    backgroundColor='#000' 
+    margin= '1%'
+    borderRadius= '10%'
+    color='#000'/>
+    </View>
+       
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  inputs:{
+   height:'5%',
+   width:'300%',
+   marginBottom:25,
+   borderBottomWidth:2,
+  },
+
+});
